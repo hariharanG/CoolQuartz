58e0182d4f147dcf2757d839905c75c9261a89a4
hariharanG <hariharan.ganpathy@gmail.com>
2017-05-30 21:21:49 +0530

    UI-Skeleton for Management of Job, Scheduler and Info is created

--- cool-quartz-web/src/main/webapp/app/js/app.js
+++ cool-quartz-web/src/main/webapp/app/js/app.js
@@ -5,6 +5,13 @@ angular.module('coolApp', [
 ]).config(['$routeProvider', function($routeProvider){
 	$routeProvider.when('/cool-job-history', {templateUrl: 'app/views/job-history.html', controller: 'JobHistoryController'});
 	$routeProvider.when('/cool-job-admin', {templateUrl: 'app/views/job-admin.html', controller: 'JobAdminController'});
+	
+	//Scheduler management aspect routing
+	$routeProvider.when('/cool-mgmt-scheduler', {templateUrl: 'app/views/scheduler-admin.html', controller: 'SchedulerAdminController'});
+	$routeProvider.when('/cool-mgmt-job', {templateUrl: 'app/views/scheduler-job-mgmt-info.html', controller: 'JobMgmtController'});
+	$routeProvider.when('/cool-mgmt-info', {templateUrl: 'app/views/scheduler-info.html', controller: 'SchedulerInfoController'});
+	
+	
 	//$routeProvider.when('/cool-job-admin', {templateUrl: 'app/views/job-admin.html'});
 	$routeProvider.otherwise({redirectTo: '/cool-job-history'});
 	
